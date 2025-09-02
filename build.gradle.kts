2563114dde38d60b3526953078c31e6b9f55868a
Leslie Rothanburg <Bill.Rothanburg@gmail.com>
2025-09-01 17:15:34 -0700

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    id("java")
+    id("xyz.jpenilla.run-paper") version "2.3.1"
+}
+
+group = "io.github.brothanb"
+version = "1.0-SNAPSHOT"
+
+var mcversion = "1.21.8"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
+}
+
+tasks {
+    processResources {
+        val props = mapOf(
+            "version" to version,
+            "mcversion" to mcversion,
+        )
+        inputs.properties(props)
+        //filteringCharset("UTF-8")
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+    }
+
+    jar {
+        manifest {
+            attributes["paperweight-mappings-namespace"] = "mojang"
+        }
+    }
+
+    runServer {
+        // Configure the Minecraft version for our task.
+        // This is the only required configuration besides applying the plugin.
+        // Your plugin's jar (or shadowJar if present) will be used automatically.
+        minecraftVersion(mcversion)
+    }
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
---
+++ src/main/java/io/github/brothanb/tphome/TpHome.java
@@ -0,0 +1,94 @@
+package io.github.brothanb.tphome;
+
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.arguments.ArgumentType;
+import io.papermc.paper.plugin.lifecycle.event.LifecycleEventManager;
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
+import static io.papermc.paper.command.brigadier.argument.ArgumentTypes.player;
+import static io.papermc.paper.command.brigadier.Commands.argument;
+import static io.papermc.paper.command.brigadier.Commands.literal;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.Commands;
+//import io.papermc.paper.command.brigadier.argument.resolvers.selector;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.configuration.file.FileConfiguration;
+import java.util.logging.Logger;
+
+public class TpHome extends JavaPlugin implements Listener {
+
+    public boolean restrict_tphome;
+    private Logger logger;
+
+    @Override
+    public void onEnable() {
+        logger = getLogger();
+        saveDefaultConfig();
+
+        FileConfiguration configuration = getConfig();
+        restrict_tphome = configuration.getBoolean("restrict_tphome");
+        if(restrict_tphome) {logger.info("restrict_tphome: true");};
+
+        this.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, commands -> {
+            final LiteralArgumentBuilder<CommandSourceStack> tphomeBuilder = Commands.literal("tphome");
+            tphomeBuilder.executes(ctx -> {
+                CommandSender sender = ctx.getSource().getSender(); // Retrieve the command sender
+                Entity executor = ctx.getSource().getExecutor(); // Retrieve the command executor, which may or may not be the same as the sender
+                //logger.info("tphome command fired.");
+                //logger.info("sender = " + sender.getName());
+                //logger.info("executor = " + executor.getName());
+                Player player;
+                if (executor instanceof Player) {
+                    player = (Player) executor;
+                    //logger.info("player = " + player.getName());
+                    Location home = player.getRespawnLocation();
+                    if (home == null) {
+                        logger.info(player.getName() + " has no home. Sending to spawn.");
+                        player.sendMessage("You have no home and will be sent to spawn.");
+                        home = executor.getServer().getWorlds().get(0).getSpawnLocation();
+                    }
+                    player.teleport(home);
+                    return 1;
+                }
+                return 0;
+            });
+
+            if (restrict_tphome) {
+                tphomeBuilder.requires(sender -> sender.getSender().hasPermission("tphome.home") );
+            }
+
+//            final LiteralArgumentBuilder<CommandSourceStack> playerArg =
+//            playerArg.requires(source -> source.getSender().isOp());
+//            playerArg.executes(ctx -> {
+//                PlayerSelectorArgumentResolver playerResolver = ctx.getArgument("player", PlayerSelectorArgumentResolver.class);
+//                Player player = targetResolver.resolve(ctx.getSource()).getFirst();
+//                Entity executor = ctx.getSource().getExecutor(); // Retrieve the command executor, which may or may not be the same as the sender
+//
+//                //logger.info("player = " + player.getName());
+//                Location home = player.getRespawnLocation();
+//                if (home == null) {
+//                    logger.info(player.getName() + " has no home. Sending to spawn.");
+//                    executor.sendMessage(player.getName() + " has no home. Sending to spawn.");
+//                    player.sendMessage("You have no home and will be sent to spawn.");
+//                    home = executor.getServer().getWorlds().get(0).getSpawnLocation();
+//                }
+//                player.teleport(home);
+//                return 1;
+//            });
+//
+//            tphomeBuilder.then(playerArg);
+
+            LiteralCommandNode tphomeNode = tphomeBuilder.build();
+            commands.registrar().register(tphomeNode, "Teleport Player Home");
+        });
+
+    }
+
+}
